d0fdb3b4143d5f440524ac3088c48e14eeac75bb
Ofir Dayan <ofird79@gmail.com>
2021-12-05 13:38:59 +0200

    added router

--- reacttemplate/src/App.jsx
+++ reacttemplate/src/App.jsx
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./component/Navbar";
+import Login from './pages/Login';
+import About from './pages/About';
+import Contact from './pages/Contact';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={Login} />
+            <Route path="/about" component={About} />
+            <Route path="/contact" component={Contact} />
+          </Switch>
+        </Router>
   );
 }
 
